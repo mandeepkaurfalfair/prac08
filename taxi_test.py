6986ce59f62986da333ce31e77af24fac3da1752
Manu <mandeepkaur.falfair@my.jcu.edu.au>
2016-09-19 12:14:28 +1000

    check complete task

---
+++ taxi_test.py
@@ -0,0 +1,22 @@
+from taxi import Taxi
+from taxi import Car
+from random import randint
+
+prius = Taxi("Prius 1", 100, 1.20)
+
+prius.drive(40)
+print(prius)
+prius.start_fare()
+print(prius)
+
+
+
+unreliableCar = Car ("UnreliableCar", 100)
+unreliableCar.drive(100)
+print(unreliableCar)
+    if unreliableCar <= 100:
+        print(reliability)
+
+
+
+
